927ed6e6c89cb74f76ae77d12429f32eb1a9aeb9
S57863B <i349cjhswe@gmail.com>
2026-05-14 22:05:52 -0400

    Added logic to movieRoutes.js

---
+++ routes/movieRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { searchMovies, getMovieDetails } = require('../controllers/movieController');
+
+// GET /api/search?title=...
+router.get('/search', searchMovies);
+
+// GET /api/movies/:id
+router.get('/movies/:id', getMovieDetails);
+
+module.exports = router;
